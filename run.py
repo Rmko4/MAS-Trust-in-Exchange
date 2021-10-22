27c0cd251ff1ddcf82b57c1d794cf6e3b6a7c90c
Marieke <marieke@kopmels.com>
2021-10-22 12:21:29 +0200

    Choose agenttype

--- run.py
+++ run.py
@@ -13,7 +13,19 @@ mob_rate = 0.1
 print("Number of agents: " + str(N))
 print("Neighbourhood size: " + str(n))
 print("Mobility rate: " + str(mob_rate))
-model = PDTModel(AgentClass="RLAgent", N=N, neighbourhood_size=n, mobility_rate=mob_rate, learning_rate=0.02, relative_reward=False)
+
+agent_class = input("\nChoose agentclass (Default is RLAgent):\n 1: MSAgent, 2: WHAgent, 3: RLAgent.\n\n")
+if agent_class == "1":
+    agent_class = "MSAgent"
+    model = PDTModel(AgentClass="MSAgent", N=N, neighbourhood_size=n, mobility_rate=mob_rate)
+elif agent_class == "2":
+    agent_class = "WHAgent"
+    model = PDTModel(AgentClass="WHAgent", N=N, neighbourhood_size=n, mobility_rate=mob_rate)
+else:
+    agent_class = "RLAgent"
+    model = PDTModel(AgentClass="RLAgent", N=N, neighbourhood_size=n, mobility_rate=mob_rate, learning_rate=0.02, relative_reward=False)
+
+print("Chosen agent type: " + agent_class)
 # print([a.neighbourhood for a in model.schedule.agents])
 model.run_model(100, 1000)
 df = model.datacollector.get_model_vars_dataframe()
